7070ef98a104d7bec55dee559159d409c0db45b4
chenshaojiecsj163 <chenshaojiecsj163@gmail.com>
2018-01-08 14:55:21 +0800

    首次上传

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "beego_pro_2/beego_pro_2/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"beego_pro_2/beego_pro_2/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
